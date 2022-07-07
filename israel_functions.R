2899db3bf568847d84a81074867585d8c7e39492
moppywhip <bkzhao38631@gmail.com>
2022-07-07 17:00:54 -0400

    adapted seir_1 function in israel_functions.R

--- israel_functions.R
+++ israel_functions.R
@@ -81,6 +81,26 @@ sir_1 = function(beta, gamma, I0, R0, times, N, lambda, mu) {
   return(as.data.frame(out))
 }
 
+seir_1 = function(beta, gamma, sigma, E0, I0, R0, times, N, lambda, mu) {
+  # define SIR equations
+  sir_equations = function(time, variables, parameters) {
+    with(as.list(c(variables, parameters)), {
+      dS = -beta * I * S/N + lambda * N - mu * S 
+      dE = beta * I * S/N - sigma * E - mu * E
+      dI =  sigma*E - gamma * I -mu * I
+      dR =  gamma * I - mu*R
+      return(list(c(dS, dE, dI, dR)))
+    })
+  }
+  # prepare input for ODE solver
+  parameters_values = c(beta = beta, gamma = gamma, sigma = sigma)
+  S0 = N - I0 - R0 - E0
+  initial_values = c(S = S0, I = I0, R = R0, E = E0)
+  # solve system of ODEs
+  out = ode(initial_values, times, sir_equations, parameters_values,method = "rk4")
+  return(as.data.frame(out))
+}
+
 
 # Calculate sums of squares ----
 ss = function(beta, gamma, N, data, lambda, mu) {
