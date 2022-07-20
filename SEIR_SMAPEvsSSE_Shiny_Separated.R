c3c6b712e8c48e052b4937266d665468287741f0
Park Szachta <93099643+parkszachta@users.noreply.github.com>
2022-07-20 15:29:39 -0400

    Add files via upload

---
+++ SEIR_SMAPEvsSSE_Shiny_Separated.R
@@ -0,0 +1,67 @@
+library(shiny)
+
+ui <- fluidPage(
+
+  # Application title
+  titlePanel("Israel COVID-19 Dashboard"),
+
+  # Show a plot of the generated distribution
+  mainPanel(
+    h4("This uses the Susceptible, Exposed, Infectious, and Removed (SEIR) framework
+        to model the number of Infectious individuals over time."),
+    h4("This uses the Susceptible, Exposed, Infectious, and Removed (SEIR) framework
+        to model the number of Removed individuals over time. This includes people
+        who have recovered from COVID-19, in addition to those who died from it."),
+    h4("This is a plot of the Test Positivity Rate (TPR) in Israel, which is the
+       proportion of COVID-19 tests that were positive. The red plot is from
+       simply dividing the numbers, and the blue plot is the reported data, implying
+       a smoothing function was used. The proportion of people who are fully vaccinated
+       is provided in green for reference."),
+    h4("This is a model of the Time-Varying R (TVR) graph, which represents the
+       expected number of secondary infections that will result from
+       an Infectious person. An R(t) value is less than 1, it is likely a sign of
+       decreasing cases, and an R(t) value of greater than 1 is a likely a sign of
+       increasing cases."),
+    tabsetPanel(type = "tabs",
+                tabPanel("Infected", plotOutput("infected_plot")),
+                tabPanel("Removed", plotOutput("removed_plot")),
+                tabPanel("TPR", plotlyOutput("tpr_plot")),
+                tabPanel("TVR", plotlyOutput("tvr_plot"))
+    ),
+    p("Cases Data: https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"),
+    p("Deaths Data: https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"),
+    p("Recovery Data: https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
+  )
+)
+
+
+
+
+
+server <- function(input, output) {
+
+load("output/model_results.RData")
+
+
+
+  output$infected_plot <- renderPlot({
+    results[[1]]
+  })
+  output$removed_plot <- renderPlot({
+    results[[2]]
+  })
+  output$tpr_plot <- renderPlotly({
+    results[[3]]
+  })
+  output$tvr_plot <- renderPlotly({
+    results[[4]]
+  })
+
+
+
+}
+
+
+
+# Run the application
+shinyApp(ui = ui, server = server)
